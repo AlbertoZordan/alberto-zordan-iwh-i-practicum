84ffdeba7c24121660f6be5ef07d7211badb0ba4
Alberto Zordan <alberto.zordan@exelab.com>
2025-03-21 12:26:22 +0100

    updated homepage route and template for data rendering

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 const HS_API_BASE_URL = "https://api.hubspot.com";
-const CO_OBJECT_TYPE = "2-42239005";
+const HS_OBJECT_TYPE = "2-42239005";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get("/", async (req, res) => {
-  res.render("homepage", { title: "Anime Characters | Homepage" });
+  const animeCharacters = `${HS_API_BASE_URL}/crm/v3/objects/${HS_OBJECT_TYPE}?properties=name,anime,age`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(animeCharacters, { headers });
+    const data = resp.data.results;
+    res.render("homepage", { title: "Anime Characters | Homepage", data });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
@@ -31,7 +42,7 @@ app.get("/update-cobj", (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 app.post("/update-cobj", async (req, res) => {
-  const createUrl = `${HS_API_BASE_URL}/crm/v3/objects/${CO_OBJECT_TYPE}`;
+  const createUrl = `${HS_API_BASE_URL}/crm/v3/objects/${HS_OBJECT_TYPE}`;
   const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     "Content-Type": "application/json",
